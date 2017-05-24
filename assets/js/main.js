f13ce286fdc588bb97dec65821b7c53a9474049d
sagicary <sagicary@gmail.com>
2017-05-24 09:45:43 -0400

    Arreglada validación js telefono

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,12 @@
 var numero=document.getElementById('botoninput');
 numero.addEventListener('click', function(){
 	var num=document.getElementById('input').value;
-	if(num.length!=9){
+	if(isNaN(num)==true){
+		alert ("Sólo puedes ingresar números");
+	}else if(num.length!=9){
 		alert("Debe ingresar un número de 9 dígitos");
 	}else{
 		var text=document.getElementById('input');
-		text.innerHTML = "Bien";
 		alert("Su número se ha registrado correctamente");
 	}
 });

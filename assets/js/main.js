72df436e1efb8720a2ec34c94773df08da021dcb
Sagicary <sagicary@gmail.com>
2017-05-24 01:54:59 -0400

    Js de telefono e ingreso de coordenadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,20 @@ numero.addEventListener('click', function(){
 		alert("Su número se ha registrado correctamente");
 	}
 });
+
+var juego=document.getElementById('jugar');
+juego.addEventListener('click', function(){
+	var x = document.getElementById('xcorde').value;
+	var y = document.getElementById("ycorde").value;
+	x=parseInt(x);
+	y=parseInt(y);
+	console.log(x);
+	if (isNaN(x)==true || isNaN(y)==true){
+		alert("Debes ingresar números");
+	}
+	else if(x>10 || y>6){
+		alert ("Para X debes ingresar un número entre 1 y 10 y para Y entre 1 y 6");
+	}else{
+		alert("Tu valor para 'X' fue: "+x+ " Y tu valor para 'Y' fue: "+y)
+	}
+});

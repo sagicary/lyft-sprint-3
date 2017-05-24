357e84986627d5fb2da162b18c79df5869b6d89b
Sagicary <sagicary@gmail.com>
2017-05-24 02:16:59 -0400

    Modificando JS coordenadas y probando tablero (comentado)

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,9 +21,43 @@ juego.addEventListener('click', function(){
 	if (isNaN(x)==true || isNaN(y)==true){
 		alert("Debes ingresar números");
 	}
-	else if(x>10 || y>6){
-		alert ("Para X debes ingresar un número entre 1 y 10 y para Y entre 1 y 6");
+	else if(x>10){
+		alert ("Para X debes ingresar un número entre 1 y 10");
+	}else if(y>6){
+		alert ("Para Y debes ingresar un número entre 1 y 6");
 	}else{
 		alert("Tu valor para 'X' fue: "+x+ " Y tu valor para 'Y' fue: "+y)
-	}
+		//Probando tablero de Blanca
+		//Defino el tablero
+/*var tablero = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+]
+//Ingreso el auto en la posicion 3,5
+tablero[x][y] = "A";
+
+//Obtengo el div en donde se dibujará el tablero
+var divTablero = document.getElementById("tablero");
+
+//Variables temporales
+var fila, casilla;
+
+//Recorro el arreglo para dibujar el tablero
+for(var i = 0; i<tablero.length; i++){
+  fila = document.createElement("div");
+  fila.classList.add("fila");
+  for(var j = 0; j<tablero[i].length; j++){
+    casilla = document.createElement("div");
+    casilla.innerHTML = tablero[i][j];
+    casilla.classList.add("casilla")
+    fila.appendChild(casilla);
+  }
+  divTablero.appendChild(fila);
+}*/
+
+}
 });
